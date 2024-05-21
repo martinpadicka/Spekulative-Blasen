1758f7496eadc79c1a3a221592bfa111de984900
martinpadicka <114466308+martinpadicka@users.noreply.github.com>
2024-05-21 11:58:37 +0200

    Update chowtest.py

--- chowtest.py
+++ chowtest.py
@@ -5,16 +5,12 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Assuming you're running this script in the same directory as your dataset
-file_path = 'monthly.csv'  # Use the relative path to the CSV file
+file_path = 'monthly.csv'  
 
-# Load the dataset from a local file
 data = pd.read_csv(file_path, parse_dates=['Date'], index_col='Date')
 
-# Adjust the column name 'price' to match your dataset if it's different
-column_name = 'Price'  # Replace 'price' with the correct column name from your CSV
+column_name = 'Price'  
 
-# Visualization of Gold Prices
 sns.lineplot(data=data, x=data.index, y=column_name)
 plt.title('Goldpreis')
 plt.xlabel('Jahr')
